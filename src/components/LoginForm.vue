6192ba17071895af8018c1d56e8cd4e1246298dd
Maxence B <maxence.bricault@gmail.com>
2020-01-09 17:59:54 +0100

    refacto(form): validate data after user types

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,11 +4,11 @@
       <header class="loginform__header">LOGIN</header>
       <label for="loginform-username" class="loginform__label">Username</label>
       <input id="loginform-username" class="loginform__input" v-model="username"
-             type="text" placeholder="Enter username">
+             type="text" placeholder="Enter username" v-on:keyup="validateUsername">
       <label for="loginform-password" class="loginform__label">Password</label>
       <input id="loginform-password" class="loginform__input" v-model="password" type="password"
-             placeholder="Enter password">
-      <button @click="submit" class="loginform__submit">SUBMIT</button>
+             placeholder="Enter password" v-on:keyup="validatePassword">
+      <button @click="submit" class="loginform__submit" :disabled="!isSubmitEnabled">SUBMIT</button>
       <Notification :notification="notification" class="loginform__notification"/>
     </div>
   </form>
@@ -31,37 +31,62 @@ export default {
       type: null,
       message: null,
     },
+    usernameTimeout: null,
+    isUsernameValid: false,
+    passwordTimeout: null,
+    isPasswordValid: false,
   }),
+  computed: {
+    isSubmitEnabled() {
+      return this.isUsernameValid && this.isPasswordValid;
+    },
+  },
   methods: {
     submit(e) {
       e.preventDefault();
-      ApiUsernameValidation(this.username)
-        .then(() => {
-          this.validatePassword()
-            .then(() => {
-              ApiSubmit({ username: this.username, password: this.password })
-                .then(() => this.setNotification(constants.NOTIFICATION_TYPE_VALID, `Congratulations ! You are now connected as ${this.username}.`))
-                .catch(error => this.setNotification(constants.NOTIFICATION_TYPE_ERROR, error));
-            })
-            .catch(error => this.setNotification(constants.NOTIFICATION_TYPE_ERROR, error));
-        })
+      ApiSubmit({ username: this.username, password: this.password })
+        .then(() => this.setNotification(constants.NOTIFICATION_TYPE_VALID, `Congratulations ! You are now connected as ${this.username}.`))
         .catch(error => this.setNotification(constants.NOTIFICATION_TYPE_ERROR, error));
     },
     setNotification(type, message) {
       this.notification.type = type;
       this.notification.message = message;
     },
+    validateUsername() {
+      clearTimeout(this.usernameTimeout);
+      this.isUsernameValid = false;
+
+      // Check if username is valid 1 second after user types
+      this.usernameTimeout = setTimeout(() => {
+        ApiUsernameValidation(this.username)
+          .then(() => {
+            this.isUsernameValid = true;
+            console.log(this.isUsernameValid);
+            this.resetNotification();
+          })
+          .catch(error => this.setNotification(constants.NOTIFICATION_TYPE_ERROR, error));
+      }, 1000);
+    },
     validatePassword() {
-      return new Promise((resolve, reject) => {
+      clearTimeout(this.passwordTimeout);
+      this.isPasswordValid = false;
+
+      // Check if password is valid 1 second after user types
+      this.passwordTimeout = setTimeout(() => {
         // Regex which test the length is at least 9 characters including 1 digit
         const passwordRegex = /^(?=.*\d).{9,}$/;
         const isPasswordValid = passwordRegex.test(this.password);
         if (isPasswordValid) {
-          resolve(isPasswordValid);
+          this.isPasswordValid = true;
+          this.resetNotification();
         } else {
-          reject(new Error('Password does not meet requirements. Your password should be longer than 8 characters and contain at least one number'));
+          this.setNotification(constants.NOTIFICATION_TYPE_ERROR, 'Password does not meet requirements. Your password should be at least 9 characters and contain at least one number');
         }
       });
+    },
+    resetNotification() {
+      this.notification.type = null;
+      this.notification.message = null;
     },
   },
 };
@@ -83,6 +108,7 @@ export default {
   padding: 2em;
 
   .loginform__header {
+    color: #41ffb4;
     margin-bottom: 10%;
     font-weight: bold;
   }
@@ -105,6 +131,10 @@ export default {
     background-color: #41ffb4;
     color: #000000;
     font-weight: bold;
+  }
+
+  .loginform__submit:disabled {
+    background-color: darkgrey;
   }
 }
 </style>
